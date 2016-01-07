7df2ce46ef58d0dbc0b6b2ee68f75a9ff4a6ff30
junioryi <tedi6410@gmail.com>
2016-01-07 15:57:06 +0800

    modify gradient func

--- matlab/gradient_of_w.m
+++ matlab/gradient_of_w.m
@@ -1,15 +1,18 @@
-function [ gw ] = gradient_of_w( M, labels, w, C )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
+function [ gw ] = gradient_of_w( M, labels, w, w_dot_x, C )
+% Compute the gradient of cost function.
+%   grad(f) = w + C * sum_i( 1/(1+exp(-ywx)) - 1) y_i * x
 
-inner_product = M .* (repmat(w, size(M,1), 1));
-product_sum = sum(inner_product, 2);
-multiply_label = labels .* product_sum;
-denominator = exp(-1*multiply_label);
-test = ones(size(denominator)) ./ (denominator + ones(size(denominator)));
-total = sum(test - ones(size(test)), 1);
+multiply_label = labels .* w_dot_x;
+denominator = exp(-1*multiply_label) + ones(size(multiply_label));
+output_size = size(denominator);
 
-disp(labels.' .* sum(M, 1));
-gw = w + C * total * (labels.' .* sum(M, 1));
+left_sum  = ones(output_size) ./ denominator - ones(output_size);
+right_sum = repmat(labels .* left_sum, 1, size(M, 2)) .* M;
+
+gw = w + C * sum( right_sum );
 end
+
+
+
+
 
--- matlab/logistic_regression_test.m
+++ matlab/logistic_regression_test.m
@@ -1,22 +1,30 @@
 fileID = fopen('output', 'w');
 
-data_file = '../data/simple_data'
+% Variables
+data_file = '../data/simple_data';
+C   = 0.1;
+eta = 0.1;
+
+% Read sparse matrix format
 [label_vector, instance_matrix] = libsvmread( data_file );
 
-% To display full matrix
-%{ 
-A = full(instance_matrix);
-disp(A);
-%}
-
-C = 0.1;
 num_feature = size(instance_matrix, 1);
 w = zeros(1, num_feature);
 w(1) = 1;
 w(2) = 1;
-[ gw ] = gradient_of_w(instance_matrix, label_vector, w, C);
 
-%formatSpec = 'Instance label: %d, features: \n';
-%out = sprintf(formatSpec, label_vector);
-%fwrite(fileID, out);
+[ col_num, row_num ] = size( instance_matrix );
+% Compute the w.T dot x, will use several time later.
+w_dot_x = sum( instance_matrix .* (repmat(w, col_num, 1)), 2 );
+
+% Compute the gradient of f
+[ gw ] = gradient_of_w(instance_matrix, label_vector, w, w_dot_x, C);
+disp(gw);
+[ ak ] = find_ak(w, gw, eta);
+
 fclose(fileID);
+
+
+
+
+
